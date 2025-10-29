ab0cdb9088bc70d86981265f0eca0b7cb13156c2
red-shinku <3480198982@qq.com>
2025-10-29 23:13:29 +0800

    初版完成

--- 并发/银行家算法/Banker.cpp
+++ 并发/银行家算法/Banker.cpp
@@ -1,33 +1,97 @@
-#include "Banker.h"
+#include "Banker.hpp"
 
 bool Banker::request(int PID, int need)
 {
-    int index = PID_func_index[PID];
-    if(need + each_now_have[index] > each_max_request[index])
+    std::unique_lock<std::mutex> lock(mtx);
+    Details& details = PID_func_details[PID];
+    if(need + details.now_have > details.max_request)
     {
         return false;
     }
     if(need > available_src)
     {
-        each_now_need[index] = need;
+        details.now_need = need;
         return false;
     }
     available_src -= need;
-    each_now_have[index] += need;
+    details.now_have += need;
     if(is_safe(PID))
     {
-        each_now_need[index] = 0;
+        details.now_need = 0;
         return true;
     }
     else
     {
         available_src += need;
-        each_now_have[index] -= need;
+        details.now_have -= need;
         return false;
     }
 }
 
 bool Banker::is_safe(int PID)
 {
-    
+    int available = available_src;
+    DetailsQueue pqueue(detailsQueue);
+    while (!pqueue.empty())
+    {
+        if(test_and_remove())
+        {
+            continue;
+        }
+        int cur_thread = pqueue.top();
+        Details &details = PID_func_details[cur_thread];
+        if(available < details.now_need)
+        {
+            return false;
+        }
+        available += details.now_have;
+        pqueue.pop();
+    }
+    return true;
+}
+
+void Banker::update_src(int PID, int nums = 0)
+{
+    std::unique_lock<std::mutex> lock(mtx);
+    auto details = PID_func_details[PID];
+    if(nums == 0)
+    {
+        available_src += details.now_have;
+        details.now_have = 0;
+    }
+    else if(nums <= details.now_have && nums > 0)
+    {
+        available_src += nums;
+        details.now_have -= nums;
+    }
+    else
+    {
+        return;
+    }
+}
+
+void Banker::add_thread(int PID, int max_request)
+{
+    std::unique_lock<std::mutex> lock(mtx);
+    PID_func_details[PID] = {max_request, 0, 0};
+    detailsQueue.push(PID);
+}
+
+bool Banker::test_and_remove()
+{
+    if(waitTORemove.top() != detailsQueue.top())
+    {
+        return false;
+    }
+    if(PID_func_details.count(waitTORemove.top()) == 0)
+    {
+        detailsQueue.pop();
+        waitTORemove.pop();
+        return true;
+    }
+    else
+    {
+        waitTORemove.pop();
+        return false;
+    }
 }
---
+++ 并发/银行家算法/Banker.hpp
@@ -0,0 +1,144 @@
+#ifndef BANKER_H
+#define BANKER_H
+
+#include <vector>
+#include <map>
+#include <queue>
+#include <functional>
+#include <mutex>
+
+/**
+ * Details：保存线程资源使用情况
+ * 每个注册线程拥有一个该结构体
+ * 成员变量：
+ *     max_request：该线程允许的最大资源数
+ *     now_have：该线程当前持有的资源
+ *     now_need：该线程当前需要的资源
+ */
+
+struct Details;
+
+/**
+ * Banker类（死锁预防）
+ * 在线程申请资源前，检查是否会出现死锁
+ * 注册了一组使用同一批资源的线程id
+ * 申请资源的线程调用request方法，若安全，
+ * 返回true允许申请，否则返回false拒绝。
+ * 释放资源的线程需要调用update_src方法，更新
+ * 资源使用信息。
+ * 该类以禁止拷贝操作，未定义移动操作
+ * 请在堆内存上创建该对象。
+ * 
+ * 成员变量：
+ *     私有：
+ *     mtx：同步原语，互斥量，控制该对象的入口
+ *     total_src：总的资源数
+ *     available_src：当前可用资源数
+ *     PID_func_details：线程id到线程Details的映射
+ *     cmp_func：可调用对象，用于优先队列的比较方法
+ *     detailsQueue：实时维护的，线程资源信息的优先队列。
+ *              内部存储的是线程id，通过map映射到相关数据结构
+ *     waitTORemove：
+ *           待删除PID队列，带优先级。辅助移除detailsQueue
+ *           中的PID。
+ * 
+ * 成员函数：
+ *     私有：
+ *     is_safe(int PID)：
+ *          检查分配资源后，线程是否安全
+ *     test_and_remove(int PID)：
+ *          检查待删队列内的PID是否能移除。若
+ *          有实际数据位于映射表，则不能移除。                       
+ * 
+ *     对外：
+ *     request(int PID, int need)：
+ *          线程请求资源时调用。先预分配，检查是否安全。
+ *     update_src(int PID, int nums):
+ *          线程释放资源时调用。更新其资源持有信息。
+ *     add_thread(int PID, int max_request)：
+ *          注册新的线程id，提供对该线程的死锁检查服务。
+ *     remove(K&&... pid)：
+ *          取消对线程的检查服务。使用可变参数模版。
+ *          同时提供对给定实参合法性的检查。若不合法则
+ *          忽略。若合法，添加至待删除队列，并从
+ *          PID_func_details中移除。
+ */
+
+class Banker 
+{
+public:
+    Banker(int thread_nums, int src_nums, std::vector<std::vector<int>>&);
+    Banker(const Banker&) = delete;
+    Banker& operator=(const Banker&) = delete;
+    ~Banker() = default;
+
+private:
+    using DetailsQueue = 
+    std::priority_queue<
+        int, 
+        std::vector<int>,
+        std::function<bool(int, int)>
+    >;
+
+    std::mutex mtx;
+    int total_src;
+    int available_src;
+    std::map<int, Details> PID_func_details;
+
+    std::function<bool(int, int)> cmp_func = 
+    [this](int PID1, int PID2) -> bool{
+        return PID_func_details[PID1].now_need < 
+            PID_func_details[PID2].now_need;
+    };
+    DetailsQueue detailsQueue;
+    DetailsQueue waitTORemove;
+
+    bool is_safe(int PID);
+    bool test_and_remove();
+    void reset_each_now_have(std::vector<int>& have);
+
+public:
+    bool request(int PID, int need);
+    void update_src(int PID, int nums);
+    void add_thread(int PID, int max_request);
+    template<typename... K> void remove(K&&... PID);
+};
+
+Banker::Banker(
+    int thread_nums,
+    int src_nums,
+    std::vector<std::vector<int>> &threads
+):
+    total_src(src_nums),
+    available_src(src_nums),
+    detailsQueue(cmp_func)
+{
+    for(auto thread: threads)
+    {
+        PID_func_details.emplace(thread[0], thread[1]);
+        detailsQueue.push(thread[0]);
+    }
+}
+
+template<typename... K>
+void Banker::remove(K&&... PID)
+{
+    auto remove_thread = [&](auto&& pid){
+        if(PID_func_details.count(pid) == 1)
+        {
+            waitTORemove.push(pid);
+            PID_func_details.erase(pid);
+        }
+    }
+
+    (remove_thread(PID), ...)
+}
+
+struct Details
+{
+    int max_request;
+    int now_have;
+    int now_need;
+};
+
+#endif //BANKER_H
